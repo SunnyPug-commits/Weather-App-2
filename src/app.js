9ff658fadead054e92ceb085e58ae90b055a022c
SunnyPug-commits <mollyhoward.ak@gmail.com>
2023-07-29 14:54:36 -0800

    added wind and humidity api

--- src/app.js
+++ src/app.js
@@ -2,14 +2,20 @@ function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
-let query = "Lisbon";
+let query = "Tampa";
 
 let apiKey = `17t909018ba21db35adf4700b4co23a4`;
 
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
